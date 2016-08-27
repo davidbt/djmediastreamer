9fec3611be2a3027ea4a06cfa0d337b51c0eb842
David Barajas <davidbt@gmail.com>
2016-08-26 19:59:34 -0500

    Don't sort any column by default in mediafiles page. The backend already ordered the data.

--- djmediastreamer/static/djmediastreamer/js/mediafiles.js
+++ djmediastreamer/static/djmediastreamer/js/mediafiles.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('#mediafiles').DataTable({
-    "lengthMenu": [[-1, 100, 50, 10], ["All", 100, 50, 10]]
+    "lengthMenu": [[-1, 100, 50, 10], ["All", 100, 50, 10]],
+    "order": []
   });
 });
