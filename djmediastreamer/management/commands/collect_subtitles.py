e50fdf664932045e62987332e0694130115a3aed
David Barajas <david@netlandish.com>
2017-02-06 13:32:46 -0600

    Collect subtitles inside mkv files.

--- djmediastreamer/management/commands/collect_subtitles.py
+++ djmediastreamer/management/commands/collect_subtitles.py
@@ -1,7 +1,9 @@
 import os
 import datetime
+import subprocess
 
 import pysrt
+import enzyme
 from django.conf import settings
 from django.db import transaction
 from django.core.management.base import BaseCommand
@@ -35,16 +37,18 @@ class Command(BaseCommand):
         lang = self.languages.get(lang_key)
         return lang
 
-    def collect_subtitles_lines(self, subtitle_file):
-        with transaction.atomic():
+    def collect_subtitles_lines(self, subtitle_file, file_path=None):
+        if not file_path:
             file_path = os.path.join(
                 subtitle_file.directory, subtitle_file.file_name)
+        with transaction.atomic():
             try:
                 subs = pysrt.open(file_path)
             except UnicodeDecodeError:
                 subs = pysrt.open(
                     file_path,
                     encoding='iso-8859-1')
+
             for sub in subs:
                 start = str(datetime.timedelta(
                     milliseconds=sub.start.ordinal))
@@ -79,7 +83,44 @@ class Command(BaseCommand):
             extension='srt',
             mediafile=rel_mediafile,
             language=lang)
+        self.collect_subtitles_lines(subtitle_file)
         return subtitle_file
+
+    def collect_mkv(self, directory, file_name):
+        full_path = os.path.join(directory, file_name)
+        with open(full_path, 'rb') as fd:
+            try:
+                mkv = enzyme.MKV(fd)
+            except Exception as e:
+                print(full_path)
+                print(e)
+                return
+            if mkv.subtitle_tracks:
+                # TODO: collect all subtitles
+                s = mkv.subtitle_tracks[0]
+                # TODO: collect S_TEXT/ASS too
+                if s.codec_id == 'S_TEXT/UTF8':
+                    temp_file = 'temp_subtitle.srt'
+                    cmd = ['mkvextract', 'tracks', full_path,
+                           '{n}:{e}'.format(n=s.number - 1, e=temp_file)]
+                    subprocess.check_output(cmd)
+                    # TODO: guess the language
+                    lang = 'simple'
+                    rel_mediafile = MediaFile.objects.filter(
+                        directory=directory,
+                        file_name=file_name).first()
+                    subtitle_file = SubtitlesFile.objects.create(
+                        file_name=file_name,
+                        directory=directory,
+                        extension='mkv',
+                        mediafile=rel_mediafile,
+                        language=lang)
+                    self.collect_subtitles_lines(subtitle_file, temp_file)
+                    os.remove(temp_file)
+                    return subtitle_file
+                else:
+                    print 'Found other codec id {id} in {f}'.format(
+                        id=s.codec_id, f=full_path)
 
     def collect_subfile(self, directory, file_name):
         subtitle_file = None
@@ -88,12 +129,9 @@ class Command(BaseCommand):
             return
 
         if file_name.lower().endswith('srt'):
-            subtitle_file = self.collect_srt(directory, file_name)
-        else:
-            pass
-            # TODO: collect mkv files
-        if subtitle_file:
-            self.collect_subtitles_lines(subtitle_file)
+            self.collect_srt(directory, file_name)
+        elif file_name.lower().endswith('mkv'):
+            self.collect_mkv(directory, file_name)
 
     def handle(self, *args, **options):
         ignore_directories = Directory.objects.filter(ignore=True)
